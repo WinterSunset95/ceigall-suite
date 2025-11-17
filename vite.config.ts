8cd6ddaae2fd1385758272569d6adba2532ffd91
WinterSunset95 <wintersunset95@gmail.com>
2025-11-17 15:31:01 +0530

    fix: changed port and host for vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
     middlewareMode: false,
     allowedHosts: [".ngrok-free.app", ".lhr.life"],
     proxy: {
